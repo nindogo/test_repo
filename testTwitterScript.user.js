b2447146ad8dbeb1fa6de498518f6a9ec4667061
nindogo <nindogo@gmail.com>
2014-07-25 22:48:07 +0300

    Update testTwitterScript.user.js

--- testTwitterScript.user.js
+++ testTwitterScript.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name      test Twitter UserScript
-// @namespace  http://use.i.E.your.homepage/
+// @namespace  http://nindogo0.tumblr.com/
 // @version    0.1
-// @description  enter something useful
-// @match      http://*/*
-// @copyright  2012+, You
+// @description  Change the redirections by vineapp to stop them having the last part
+// @match      https://twitter.com/intent/tweet?*
+// @copyright  2014+ Nindogo
 // ==/UserScript==
 
 alert("Hello, World!");
