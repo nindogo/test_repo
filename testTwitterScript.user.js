7d42b9224319eb5f63569de929ecb3b542e2e66a
Ni Ndogo <nindogo@gmail.com>
2014-07-25 19:18:16 +0000

    Nothing to say

---
+++ testTwitterScript.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name      test Twitter UserScript
+// @namespace  http://use.i.E.your.homepage/
+// @version    0.1
+// @description  enter something useful
+// @match      http://*/*
+// @copyright  2012+, You
+// ==/UserScript==
+
+alert("Hello, World!");
