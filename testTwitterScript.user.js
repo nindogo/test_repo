36d7300b8b13833c9431c12b9c2ff2eea6d6360e
nindogo <nindogo@gmail.com>
2014-07-28 01:06:17 +0300

    Update testTwitterScript.user.js
    Added changes to make the twitting now automatic.

--- testTwitterScript.user.js
+++ testTwitterScript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name      test Twitter UserScript
 // @namespace  http://nindogo0.tumblr.com/
-// @version    0.2.0.3
+// @version    0.2.0.4
 // @run-at document-start
 // @description  Change the redirections by vineapp to stop them having the last part
 // @match      https://twitter.com/intent/tweet?*
@@ -10,10 +10,16 @@
 // @author nindogo
 // ==/UserScript==
 
-var x,y;
+var x,y,z;
 x = document.location.href;
 
 if (x.search("via=vineapp") > -1){
-    y=x.replace("vineapp","");
+    y=x.replace("via=vineapp","");
     document.location.replace(y);
+}
+
+if (!(x.search("via=vineapp") > -1)){
+    z=document.getElementById("update-form");
+    console.log(z);
+    z.submit();
 }
