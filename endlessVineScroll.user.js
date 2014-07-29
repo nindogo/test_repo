ebeaad5b199cb70e71de2ecc284b1f50eacba53a
nindogo <nindogo@gmail.com>
2014-07-29 23:25:08 +0300

    Create endlessVineScroll.user.js

---
+++ endlessVineScroll.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name       Scroll Vine 
+// @namespace  http://nindogo.tumblr.com/
+// @version    0.1
+// @description  vine Auto Scroll
+// @match      https://vine.co/*
+// @downloadURL https://github.com/nindogo/test_repo/raw/master/endlessVineScroll.user.js
+// @run-at  document-end
+// @copyright  2014+, Nindogo
+// ==/UserScript==
+
+var x,i;
+i=200;
+window.onscroll = scrollMore;
+
+function scrollMore() {
+    if (document.body.scrollTop + window.innerHeight >= (document.body.scrollHeight - i)) {
+        x = document.getElementsByClassName("pagination");
+        if (x.length == 1) {
+            x[0].click();}
+    }
+}
