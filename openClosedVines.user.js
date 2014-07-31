8610dfc134228dde6d8637e8e24db8cd223176e4
nindogo <nindogo@gmail.com>
2014-07-31 20:36:04 +0300

    Create openClosedVines.user.js
    New userscript.

---
+++ openClosedVines.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name       Open Closed Vines 
+// @namespace  http://nindogo.tumblr.com/
+// @version    0.1.0.1
+// @description  vine Auto Scroll
+// @match      https://vine.co/*
+// @downloadURL https://github.com/nindogo/test_repo/raw/master/openClosedVines.user.js
+// @run-at  document-end
+// @copyright  2014+, Nindogo
+// ==/UserScript==
+
+var x,i;
+
+window.onscroll = openVines;
+x= document.getElementsByClassName("small");
+
+function openVines() {
+    for (i = (x.length-1); i > -1; i--) {
+        console.log(i);
+        console.log(x[i]);
+        try{
+            x[i].click();  
+        }
+        catch (error){
+            console.log("during "+i+" I caught error: "+error.type+" with message: "+error.message);
+        }
+    }
+}
