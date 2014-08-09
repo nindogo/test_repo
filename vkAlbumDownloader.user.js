06d37b81602961847a057c57c17e70522f952983
nindogo <nindogo@gmail.com>
2014-08-09 23:12:26 +0300

    Update vkAlbumDownloader.user.js

--- vkAlbumDownloader.user.js
+++ vkAlbumDownloader.user.js
@@ -14,7 +14,7 @@
 // @grant         GM_xmlhttpRequest
 // @version       0.4.0.1
 // @require       http://raw.github.com/vuplea/tiny_zip_js/master/tiny_zip.js
-// @downloadURL   http://userscripts.org/scripts/source/177383.user.js
+// @downloadURL   http://raw.github.com/nindogo/test_repo/vkAlbumDownloader.user.js
 // ==/UserScript==
 
 if (GM_getValue("switch") == undefined) GM_setValue("switch", "on");
