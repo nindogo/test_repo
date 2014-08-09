25f6465ea12a17e3a4ad766d235e873f555f86e3
nindogo <nindogo@gmail.com>
2014-08-09 20:23:00 +0300

    Update autoTwitterIntentScript.user.js

--- autoTwitterIntentScript.user.js
+++ autoTwitterIntentScript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name      test Twitter UserScript
+// @name      Auto Twitter Intent Script - removing via
 // @namespace  http://nindogo.tumblr.com/
-// @version    0.2.0.4
+// @version    0.2.0.5
 // @run-at document-start
 // @description  Change the redirections by vineapp to stop them having the last part
 // @match      https://twitter.com/intent/tweet?*
