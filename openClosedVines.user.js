b21daf4a924f1ddd7e3cd6e67bfbcf60d13502e9
nindogo <nindogo@gmail.com>
2014-08-25 01:07:39 +0300

    Update openClosedVines.user.js
    Reworking it!

--- openClosedVines.user.js
+++ openClosedVines.user.js
@@ -1,29 +1,28 @@
 // ==UserScript==
-// @name       Open Closed Vines 
+// @name       open sensitive Vines
 // @namespace  http://nindogo.tumblr.com/
-// @version    0.1.0.2
-// @description  vine Auto Scroll
+// @version    0.1.0.3
+// @description  open vines that are hidden due to sensitivity.
 // @match      https://vine.co/*
 // @downloadURL https://github.com/nindogo/test_repo/raw/master/openClosedVines.user.js
 // @run-at  document-end
 // @copyright  2014+, Nindogo
 // ==/UserScript==
 
-var x,i;
 
-window.onscroll = openVines;
-
+window.onload = openVines;
 
 function openVines() {
-    x= document.getElementsByClassName("small");
-    for (i = (x.length-1); i > -1; i--) {
+    var i, j, x, y, z;
+    x = document.getElementsByClassName("small");
+    console.log(x.length);
+    for (i = (x.length - 1); i > -1; i--) {
         console.log(i);
         console.log(x[i]);
-        try{
-            x[i].click();  
-        }
-        catch (error){
-            console.log("during "+i+" I caught error: "+error.type+" with message: "+error.message);
+        try {
+            x[i].click();
+        } catch (error) {
+            console.log("during " + i + " I caught error: " + error.type + " with message: " + error.message);
         }
     }
 }
