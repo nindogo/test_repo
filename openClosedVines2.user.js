d43309370f6bdc64b4dfdbefddecdcfe9b2e4a82
nindogo <nindogo@gmail.com>
2014-09-03 18:16:30 +0300

    Create openClosedVines2.user.js

---
+++ openClosedVines2.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name       Auto open sensitive Vines
+// @namespace  http://nindogo.tumblr.com/
+// @version    0.1.0.4
+// @description  open vines that are hidden due to sensitivity.
+// @match      https://vine.co/*
+// @downloadURL https://github.com/nindogo/test_repo/raw/master/openClosedVines2.user.js
+// @include https://mutation-summary.googlecode.com/git/src/mutation-summary.js
+// @run-at  document-end
+// @copyright  2014+, Nindogo
+// ==/UserScript==
+
+
+window.onload = openVines;
+
+function openVines() {
+    var i, j, x, y, z;
+    x = document.getElementsByClassName("small");
+    console.log(x.length);
+    for (i = (x.length - 1); i > -1; i--) {
+        console.log(i);
+        console.log(x[i]);
+        try {
+            x[i].click();
+        } catch (error) {
+            console.log("during " + i + " I caught error: " + error.type + " with message: " + error.message);
+        }
+    }
+}
