2040863385de87930fbacf2c3487e6d568e6df7b
nindogo <nindogo@gmail.com>
2014-09-14 15:36:42 +0300

    Update openFT.user.js

--- openFT.user.js
+++ openFT.user.js
@@ -3,6 +3,7 @@
 // @namespace  http://ft.com/
 // @version    0.1
 // @description  enter something useful
+// @downloadURL https://github.com/nindogo/test_repo/raw/master/openFT.user.js
 // @match       *://*.ft.tld/*
 // @user-agent Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)
 // @copyright  2011+, You
