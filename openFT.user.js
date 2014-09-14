bf0ac7b5c591aea1dfc4c4aff52a3d1434907e9f
nindogo <nindogo@gmail.com>
2014-09-14 15:35:33 +0300

    Create openFT.user.js

---
+++ openFT.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name       UA | FT.com
+// @namespace  http://ft.com/
+// @version    0.1
+// @description  enter something useful
+// @match       *://*.ft.tld/*
+// @user-agent Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)
+// @copyright  2011+, You
+// ==/UserScript==
