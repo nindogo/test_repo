fb3c8010e4f0d5cee176a5e7aa463cc98ef20986
nindogo <nindogo@gmail.com>
2014-09-24 20:36:58 +0300

    Verstion bump and added emoji thumbs up sign as text

--- autoTwitterIntentScript.user.js
+++ autoTwitterIntentScript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name      Auto Twitter Intent Script
 // @namespace  http://nindogo.tumblr.com/
-// @version    0.1.0.6
+// @version    0.1.0.7
 // @run-at document-start
 // @description  Automatically twits vines and removes any text in them (including the via)
 // @match      https://twitter.com/intent/tweet?*
@@ -29,7 +29,7 @@ if (w.url == w.original_referer){
 if (x.search("via=vineapp") > -1){
     u = new URI("https://twitter.com/intent/tweet");
     u.addSearch("url", w.url);
-    //u.addSearch("text", w.url);
+    u.addSearch("text","👍"�url);
     u.addSearch("original_referer", w.original_referer);
     y=u.toString();
     document.location.replace(y);
