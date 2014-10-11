fecc8494dedfed7ebf9c96f107e629e1c6d331ca
nindogo <nindogo@gmail.com>
2014-10-11 19:06:33 +0300

    Update vineRevineHider.2.user.js

--- vineRevineHider.2.user.js
+++ vineRevineHider.2.user.js
@@ -1,18 +1,22 @@
 // ==UserScript==
 // @name         vine Revine Hider on Feed
 // @namespace    http://nindogo.tumblr.com
-// @version      0.1
+// @version      0.1.1
 // @description  On Vine Feed show only vines that are original. No Revines.
 // @author       nindogo
 // @match      https://vine.co/feed*
 // @downloadURL https://github.com/nindogo/test_repo/raw/master/vineRevineHider.2.user.js
-// @grant         GM_addStyle 
+// @grant         GM_addStyle
+// @grant       unsafeWindow
+// @require     //ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
 // @run-at        document-start
 // ==/UserScript==
 
 (function () {
+    
+    jQuery(".post-byline").parent().remove();
     // class is . and id is #
     //GM_addStyle('.post-byline ~ .card  { background-color: #b0c4de; }');
-    GM_addStyle('.post-byline ~ .card  { display:none !important; }');
-    GM_addStyle('.post-byline  { display:none !important; }');
+    //GM_addStyle('.post-byline ~ .card  { display:none !important; }');
+    //GM_addStyle('.post-byline  { display:none !important; }');
 }) ();
