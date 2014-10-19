2b1aaecb0b09e1e67ce07bea85a716ccf2b5f26b
nindogo <nindogo@gmail.com>
2014-10-20 02:34:27 +0300

    Create tumblrHideProfileReblogs.user.js

---
+++ tumblrHideProfileReblogs.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name          Tumblr.com - Hide reblogs from Other Profiles
+// @namespace     https://greasyfork.org
+// @description	  This will hide any reblogs on your Tumblr dashboard.
+// @author        nindogo
+// @downloadURL https://github.com/nindogo/test_repo/raw/master/tumblrHideProfileReblogs.user.js
+// @include       http://*.tumblr.com/*
+// @include       https://*.tumblr.com/*
+// @grant         GM_addStyle 
+// @run-at        document-start
+// @version       0.01
+// ==/UserScript==
+(function () {
+    GM_addStyle('#posts.content .reblogged {display:none !important; }');
+}) ();
