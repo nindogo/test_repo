91c58b9b03472092f3dc8413142c5d098e1913c0
nindogo <nindogo@gmail.com>
2014-11-19 19:09:56 +0300

    Changed order putting excludes first

--- tumblrHideProfileReblogs.user.js
+++ tumblrHideProfileReblogs.user.js
@@ -4,13 +4,13 @@
 // @description	  This will hide any reblogs on your Tumblr dashboard.
 // @author        nindogo
 // @downloadURL https://github.com/nindogo/test_repo/raw/master/tumblrHideProfileReblogs.user.js
-// @include       http://*.tumblr.com/*
-// @include       https://*.tumblr.com/*
 // @exclude       http://www.tumblr.com/dashboard
 // @exclude       https://www.tumblr.com/dashboard
+// @include       http://*.tumblr.com/*
+// @include       https://*.tumblr.com/*
 // @grant         GM_addStyle 
 // @run-at        document-start
-// @version       0.02
+// @version       0.03
 // ==/UserScript==
 (function () {
     GM_addStyle('#posts.content .reblogged {display:none !important; }');
