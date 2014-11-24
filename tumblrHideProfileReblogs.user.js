e26e12a59d5b982d813bdbc02cca8f6c97daee47
nindogo <nindogo@gmail.com>
2014-11-24 21:52:40 +0300

    Update tumblrHideProfileReblogs.user.js
    Additional changes to see if it works.
    Having issues working only on user profiles and not on the main home.

--- tumblrHideProfileReblogs.user.js
+++ tumblrHideProfileReblogs.user.js
@@ -4,13 +4,11 @@
 // @description	  This will hide any reblogs on your Tumblr dashboard.
 // @author        nindogo
 // @downloadURL https://github.com/nindogo/test_repo/raw/master/tumblrHideProfileReblogs.user.js
-// @exclude       http://www.tumblr.com/dashboard
 // @exclude       https://www.tumblr.com/dashboard
 // @include       http://*.tumblr.com/*
-// @include       https://*.tumblr.com/*
 // @grant         GM_addStyle 
 // @run-at        document-start
-// @version       0.03
+// @version       0.04
 // ==/UserScript==
 (function () {
     GM_addStyle('#posts.content .reblogged {display:none !important; }');
