8630299df1e4d51f44e6a4784d61c2d657a5b9d0
Ni Ndogo <nindogo@gmail.com>
2018-04-13 23:03:50 +0300

    Update flickrExperiments.user.js

--- flickrExperiments.user.js
+++ flickrExperiments.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         New Userscript
-// @namespace    http://tampermonkey.net/
+// @namespace    http://nindogo.tumblr.com
 // @version      0.0.1
-// @description  try to take over the world!
+// @description  Learning userscripts and working in flickr
 // @author       Ni Ndogo
 // @updateURL    https://github.com/nindogo/test_repo/raw/master/flickrExperiments.user.js
 // @downloadURL  https://github.com/nindogo/test_repo/raw/master/flickrExperiments.user.js
