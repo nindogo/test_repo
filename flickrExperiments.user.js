7d4c855f04f05a87f21e71e20ae8a73ccedf5788
Ni Ndogo <nindogo@gmail.com>
2018-04-13 22:31:43 +0300

    Update flickrExperiments.user.js

--- flickrExperiments.user.js
+++ flickrExperiments.user.js
@@ -4,6 +4,8 @@
 // @version      0.0.1
 // @description  try to take over the world!
 // @author       Ni Ndogo
+// @updateURL    https://github.com/nindogo/test_repo/raw/master/flickrExperiments.user.js
+// @downloadURL  https://github.com/nindogo/test_repo/raw/master/flickrExperiments.user.js
 // @match        https://www.flickr.com/photos/*
 // @match        http*://www.flickr.com/photos/*
 // @grant        none
