2c0c9f7456a0bc242bc75649b826a583e0038dc4
Ni Ndogo <nindogo@gmail.com>
2019-02-20 13:41:25 +0300

    Update flickrGroupsCheckRestricted.user.js
    Cleaned up the script and changed format of the version numbers

--- flickrGroupsCheckRestricted.user.js
+++ flickrGroupsCheckRestricted.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            flickrGroupsCheckRestricted
 // @namespace       http://tampermonkey.net/
-// @version         0.2
+// @version         20190220
 // @description     try to take over the world!
 // @author          You
 // @match           https://www.flickr.com/groups/*/pool/*
@@ -10,19 +10,13 @@
 //                  Selectors and the following script require jQuery.
 // @require         https://gist.githubusercontent.com/raw/2625891/waitForKeyElements.js
 //                  The previous require is from a script of Brock Adams (Thanks to him!)
-// @downloadURL     https://github.com/nindogo/test_repo/raw/master/flickrGroupsCheckRestricted.user.js
 // ==/UserScript==
 
 waitForKeyElements(".view.photo-list-photo-view.awake", showHiddenOrOpen, bWaitOnce=false);
 
 function showHiddenOrOpen(jNode){
 
-    var t = jNode
-    var u = jNode[0]["style"]["backgroundImage"];
-    var v = u.split("/");
-    var w = v[5];
-    var x = w.split("_");
-    var y = x[0]
+    var photo_id = ((((jNode[0].style.backgroundImage).split("/"))[5]).split("_"))[0];
 
     var protocol = window.location.protocol;
     var api_key = "9f7fba1e66c150084f948ab8df0ce3a9"
@@ -31,26 +25,24 @@ function showHiddenOrOpen(jNode){
     +'?method=flickr.photos.getFavorites'
     +'&api_key=' + api_key
     +'&format=json&nojsoncallback=1'
-    +'&photo_id=' + y;
+    +'&photo_id=' + photo_id;
 
     GM_xmlhttpRequest({
         method: 'GET',
         url: url,
         context: jNode,
         onload: function(response){
-            var b = (JSON.parse(response.responseText));
-            var x = b.stat;
-            var z = response.context[0];
+            var a = (JSON.parse(response.responseText)).stat;
+            var b = response.context[0];
 
-            if (x == 'ok') {
-                z.style.border = "5px solid #39ff14";
+            if (a == 'ok') {
+                b.style.border = "5px solid #39ff14";
             }
-            else if (x == 'fail'){
-                z.style.border = "5px solid #f70000";
+            else if (a == 'fail'){
+                b.style.border = "5px solid #ff1919";
             }
 
         }
 
     })
 }
-
