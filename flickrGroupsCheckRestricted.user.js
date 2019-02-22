9b505ec08992a0965d1c8722812024d7dafa0499
Ni Ndogo <nindogo@gmail.com>
2019-02-22 13:01:43 +0300

    Added support for previous version of groups

--- flickrGroupsCheckRestricted.user.js
+++ flickrGroupsCheckRestricted.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            flickrGroupsCheckRestricted
 // @namespace       http://tampermonkey.net/
-// @version         20190220
+// @version         20190222
 // @description     try to take over the world!
 // @author          You
 // @match           https://www.flickr.com/groups/*/pool/*
@@ -13,6 +13,9 @@
 // ==/UserScript==
 
 waitForKeyElements(".view.photo-list-photo-view.awake", showHiddenOrOpen, bWaitOnce=false);
+
+waitForKeyElements(".pool-photo.photo-display-item", showHiddenOrOpen_2);
+
 
 function showHiddenOrOpen(jNode){
 
@@ -39,7 +42,41 @@ function showHiddenOrOpen(jNode){
                 b.style.border = "5px solid #39ff14";
             }
             else if (a == 'fail'){
-                b.style.border = "5px solid #ff1919";
+                b.style.border = "5px solid #F71F12";
+            }
+
+        }
+
+    })
+}
+
+function showHiddenOrOpen_2(jNode){
+
+//     var photo_id = ((((jNode[0].style.backgroundImage).split("/"))[5]).split("_"))[0];
+    var photo_id = jNode[0]["dataset"]["photoId"];
+
+    var protocol = window.location.protocol;
+    var api_key = "9f7fba1e66c150084f948ab8df0ce3a9"
+
+    var url = protocol+'//api.flickr.com/services/rest/'
+    +'?method=flickr.photos.getFavorites'
+    +'&api_key=' + api_key
+    +'&format=json&nojsoncallback=1'
+    +'&photo_id=' + photo_id;
+
+    GM_xmlhttpRequest({
+        method: 'GET',
+        url: url,
+        context: jNode,
+        onload: function(response){
+            var a = (JSON.parse(response.responseText)).stat;
+            var b = response.context[0];
+
+            if (a == 'ok') {
+                b.style.border = "5px solid #39ff14";
+            }
+            else if (a == 'fail'){
+                b.style.border = "5px solid #F71F12";
             }
 
         }
