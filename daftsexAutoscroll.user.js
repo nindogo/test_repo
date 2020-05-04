1c8e34004437cedab292f2ab0e489172cdad880c
Ni Ndogo <nindogo@gmail.com>
2020-05-04 04:16:32 +0300

    Update daftsexAutoscroll.user.js

--- daftsexAutoscroll.user.js
+++ daftsexAutoscroll.user.js
@@ -1,18 +1,19 @@
 // ==UserScript==
-// @name         Auto Scroll Daft Sex
-// @version      20200501
-// @description  autoscroll daftsex
-// @author       nindogo
-// @require      https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js
-// @require      https://daftsex.com/js/common.js
-// @match        http*://daftsex.com/*
-// @grant        none
+// @name            Auto Scroll Daft Sex
+// @version         20200501
+// @description     autoscroll daftsex
+// @author          nindogo
+// @require         https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js
+// @require         https://daftsex.com/js/common.js
+// @match           http*://daftsex.com/*
+// @grant           none
+// @downloadURL     https://github.com/nindogo/test_repo/raw/master/daftsexAutoscroll.user.js
 // ==/UserScript==
 
-var i=200;
+var sensitivity=200;
 
 $(window).scroll(function() {
-    if ($(window).scrollTop() + $(window).height() >= ($(document).height() - i)) {
+    if ($(window).scrollTop() + $(window).height() >= ($(document).height() - sensitivity)) {
         var hapa = $('div.more')
         if(hapa.length != 0) {
             more(hapa);
