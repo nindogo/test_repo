819148c10dd4a9086dc9ff0290000896bc2640b9
Ni Ndogo <nindogo@gmail.com>
2020-08-21 03:16:31 +0300

    New home page for flickr.

--- flickrPhotoShowPublicOrPrivate.user.js
+++ flickrPhotoShowPublicOrPrivate.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name            flickrPhotoShowPublicOrPrivate
 // @namespace       http://tampermonkey.net/
-// @version         20200126
+// @version         20200821
 // @description     On Flickr show whether a photo is public or private by drawing a colored box around it.
 // @author          nindogo
-// @match           https://www.flickr.com/groups/*/pool/*
+// @match           https://www.flickr.com/groups/*
 // @match           https://www.flickr.com/groups/*/pool/*/
 // @match           https://www.flickr.com/photos/*/
 // @match           https://www.flickr.com/photos/*
@@ -27,6 +27,7 @@
 //                  Selectors and the following script require jQuery.
 // @require         https://gist.githubusercontent.com/raw/2625891/waitForKeyElements.js
 //                  The previous require is from a script of Brock Adams (Thanks to him!)
+// @icon            https://combo.staticflickr.com/pw/images/favicons/favicon-180.png
 //@downloadURL      https://github.com/nindogo/test_repo/raw/master/flickrPhotoShowPublicOrPrivate.user.js
 // ==/UserScript==
 
@@ -38,6 +39,8 @@ waitForKeyElements(".activity-card-photo  ", showHiddenOrOpen_2);
 
 waitForKeyElements(".preview-img-link", showHiddenOrOpen_3);
 waitForKeyElements(".preview-video-link", showHiddenOrOpen_4);
+
+waitForKeyElements(".photo-link", showHiddenOrOpen_5 );
 // waitForKeyElements("div.videoplayer", showHiddenOrOpen_4);
 
 
@@ -162,6 +165,39 @@ function showHiddenOrOpen_4(jNode){
         onload: function(response){
             var a = (JSON.parse(response.responseText)).stat;
             var b = response.context[0].childNodes[1];
+
+            if (a == 'ok') {
+                b.style.border = "5px solid #39ff14";
+            }
+            else if (a == 'fail'){
+                b.style.border = "5px solid #F71F12";
+            }
+
+        }
+
+    })
+}
+
+// New home page. Work on link and go up to div.
+function showHiddenOrOpen_5(jNode){
+    var photo_id = jNode[0].href.split("/")[5];
+
+    var protocol = window.location.protocol;
+    var api_key = "9f7fba1e66c150084f948ab8df0ce3a9"
+
+    var url = protocol+'//api.flickr.com/services/rest/'
+    +'?method=flickr.photos.getFavorites'
+    +'&api_key=' + api_key
+    +'&format=json&nojsoncallback=1'
+    +'&photo_id=' + photo_id;
+
+    GM_xmlhttpRequest({
+        method: 'GET',
+        url: url,
+        context: jNode,
+        onload: function(response){
+            var a = (JSON.parse(response.responseText)).stat;
+            var b = response.context[0].parentNode.parentNode;
 
             if (a == 'ok') {
                 b.style.border = "5px solid #39ff14";
